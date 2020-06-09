7ad155a314cd8004149b615099c3dd2a7b6eef2a
cqrlosArch <diaz.flores.c@gmail.com>
2020-06-09 12:21:08 +0200

    09/06/2020

---
+++ build/precache-manifest.b2a3610a3afe15de9542c27336224db1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ac575d16c3bb2018ea7c7c452f5d01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b313f8dd95f4968f598",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4b313f8dd95f4968f598",
+    "url": "/static/js/2.6ee46f03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6ee46f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eb51d151ffc364d51c2",
+    "url": "/static/js/main.c31a0b75.chunk.js"
+  },
+  {
+    "revision": "f71c9d0b07a2fc89430c",
+    "url": "/static/js/runtime-main.1c746a23.js"
+  },
+  {
+    "revision": "dc8b886b4cf2a2426a6664ad17ebf7f9",
+    "url": "/static/media/bg-boost-desktop.dc8b886b.svg"
+  },
+  {
+    "revision": "a82de60e4b42d5b6b6abf66a03a2dca1",
+    "url": "/static/media/bg-shorten-desktop.a82de60e.svg"
+  },
+  {
+    "revision": "3e2a45e7afec03db9b60b3ee3438b3f7",
+    "url": "/static/media/icon-brand-recognition.3e2a45e7.svg"
+  },
+  {
+    "revision": "e3d9caa9df26426d598106f7f7289625",
+    "url": "/static/media/icon-detailed-records.e3d9caa9.svg"
+  },
+  {
+    "revision": "7febcff9f143b83553d544fc04fc5b09",
+    "url": "/static/media/icon-facebook.7febcff9.svg"
+  },
+  {
+    "revision": "cc099cea0da2f5ae13aef182c107a854",
+    "url": "/static/media/icon-fully-customizable.cc099cea.svg"
+  },
+  {
+    "revision": "996b697c33ab8772e59e3c9bec1124ea",
+    "url": "/static/media/icon-instagram.996b697c.svg"
+  },
+  {
+    "revision": "c557c3d4e80b5eda1ae120c8c2257e26",
+    "url": "/static/media/icon-pinterest.c557c3d4.svg"
+  },
+  {
+    "revision": "cb4fea0b4771a5dfc70f787d5e01c91e",
+    "url": "/static/media/icon-twitter.cb4fea0b.svg"
+  },
+  {
+    "revision": "6387840f9b6e8720165db8c73185bc51",
+    "url": "/static/media/logo.6387840f.svg"
+  }
+]);
